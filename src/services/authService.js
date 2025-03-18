52b69054e958cc9ed8ecc75834fb8c3001a61295
mohamednasser11 <mohamednaser1000.mn@gmail.com>
2025-03-18 02:58:13 +0200

    starting authentication

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,23 +1,38 @@
-import axios from "axios"
+import axios from "axios";
+import Cookies from "js-cookie";
+import jwt, { verify } from "jsonwebtoken";
 
-const BASE_URL = 'http://localhost:8000'
-const login = async (email, password, rememberMe)=> {
-    try {
-        const response = await axios.post(`${BASE_URL}/api/v1/users/login/`, {
-            email: email,
-            password: password
-        },
-        {
-            withCredentials: true, // Send cookies with the request
-        });
+const BASE_URL = "http://localhost:8000";
+const login = async (email, password, rememberMe) => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/api/v1/users/login/`,
+      {
+        email: email,
+        password: password,
+      },
+      {
+        withCredentials: true,
+      }
+    );
 
-        console.log(`>>>>> response ${JSON.stringify(response)}`)
-    } catch (error) {
-        throw new Error(error.response?.data?.message || 'Login failed');
+    console.log(`>>>>> response ${JSON.stringify(response)}`);
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Login failed");
+  }
+};
+
+const isAuthenticated = () => {
+    let isAuthenticated = false;
+    const getAccessToken = Cookies.get("token");
+    if(getAccessToken) {
+        //TODO:: add the jwt verify callback to check if the token is still valid
+        jwt.verify()
     }
-}
-
+  return getAccessToken ? true : false;
+};
 
 export default {
-    login
-}
+  login,
+  isAuthenticated
+};
